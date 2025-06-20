eefa132151a00b2f40eb22396d9877fc5eec540e
bracesproul <braceasproul@gmail.com>
2025-06-20 13:30:36 -0700

    cr

--- apps/web/src/components/v2/thread-view.tsx
+++ apps/web/src/components/v2/thread-view.tsx
@@ -205,7 +205,7 @@ export function ThreadView({
                   <div className="text-foreground text-sm leading-relaxed">
                     {getMessageContentString(message.content)}
                   </div>
-                  <div className="absolute top-0 right-0 opacity-0 group-hover:opacity-100 transition-opacity">
+                  <div className="absolute top-0 right-0 opacity-0 transition-opacity group-hover:opacity-100">
                     <MessageCopyButton
                       content={getMessageContentString(message.content)}
                     />
@@ -303,6 +303,3 @@ export function ThreadView({
     </div>
   );
 }
-
-
-
