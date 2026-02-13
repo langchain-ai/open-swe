a5b5205bb99d262b57b2efa15bd42af3f4592132
bracesproul <braceasproul@gmail.com>
2026-02-13 14:09:52 -0800

    chore: Delete unused apps code

--- apps/open-swe/src/routes/app.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { unifiedWebhookHandler } from "./github/unified-webhook.js";
-
-export const app = new Hono();
-
-app.post("/webhooks/github", unifiedWebhookHandler);
--- apps/open-swe/src/utils/should-use-custom-framework.ts
+++
@@ -1,5 +0,0 @@
-import { GraphConfig } from "@openswe/shared/open-swe/types";
-
-export function shouldUseCustomFramework(config: GraphConfig): boolean {
-  return config.configurable?.customFramework === true;
-}
