4f71a9438502d73cda517e5fbf3263ad4e88b9bf
AkhiChalasani <chalasaniakhil010@gmail.com>
2026-05-12 19:08:27 -0500

    fix-issue: improve code quality
    - Attempted fix for issue #1233: We need to add Docker as a first-class `SANDBOX_TYPE` provider. The existing codebase already has a
    
    Closes #1233
    
    Signed-off-by: AkhiChalasani <chalasaniakhil010@gmail.com>

--- agent/integrations/__init__.py
+++ agent/integrations/__init__.py
@@ -2,6 +2,12 @@
 
 from deepagents.backends import LangSmithSandbox
 
+from agent.integrations.docker import DockerSandbox, create_docker_sandbox
 from agent.integrations.langsmith import LangSmithProvider
 
-__all__ = ["LangSmithProvider", "LangSmithSandbox"]
+__all__ = [
+    "DockerSandbox",
+    "LangSmithProvider",
+    "LangSmithSandbox",
+    "create_docker_sandbox",
+]
